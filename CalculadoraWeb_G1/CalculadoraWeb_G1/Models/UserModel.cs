24ede0ada2cbaec3843394ac689fe60915ec88bb
AdrianGaray <adriangaray396@gmail.com>
2022-02-05 16:04:09 -0300

    Sea gregan nuevas carpetas

---
+++ CalculadoraWeb_G1/CalculadoraWeb_G1/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculadoraWeb_G1.Models
+{
+    public class UserModel
+    {
+        public UserModel()
+        {
+        }
+
+        [Required]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Correo electrónico")]
+        public string Email { get; set; }
+    }
+}
---
+++ CalculadoraWeb_G1/CalculadoraWeb_G1/Services/CalculateModelService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculadoraWeb_G1.Services
+{
+    public class CalculateModelService
+    {
+        public CalculateModelService()
+        {
+        }
+
+        public double CalculateResult()
+        {
+            // Acá llamamos a la API
+            return 123321;
+        }
+    }
+}
