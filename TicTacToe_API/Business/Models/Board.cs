aee63f638f8a9e19a112f6f8a8d74c9381c50744
Akilles65 <aceselli.work@gmail.com>
2022-02-08 09:27:31 -0300

    test

---
+++ TicTacToe_API/Business/Models/Board.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class Board
+    {
+        public int  Row { get; set; }
+
+        public int Col { get; set; }
+
+        public int Diag { get; set; }
+    }
+}
---
+++ TicTacToe_API/Business/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public bool Turn { get; set; }
+
+        public string ChipType { get; set; }
+
+    }
+}
