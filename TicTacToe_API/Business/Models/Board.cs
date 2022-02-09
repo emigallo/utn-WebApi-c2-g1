14ba1c4530853ef22e024b60b84a8b3b64d2e700
AdrianGaray <adriangaray396@gmail.com>
2022-02-09 15:26:42 -0300

    Miercoles

---
+++ TicTacToe_API/Business/Models/Board.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class Board
+    {
+        public Board()
+        {
+            FullEmpty();
+        }
+
+        // Posiciones del tablero
+        public TypePiece[] Positions { get; init; } // init
+        // Alex
+
+        public void FullEmpty()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Positions[i] = TypePiece.empty;
+               
+            }
+        }
+    }
+}
