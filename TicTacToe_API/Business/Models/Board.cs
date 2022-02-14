737e94bc334a85909d76a045a7074019dad87a56
AdrianGaray <adriangaray396@gmail.com>
2022-02-14 09:38:12 -0300

    Commit del lunes

--- TicTacToe_API/Business/Models/Board.cs
+++ TicTacToe_API/Business/Models/Board.cs
@@ -11,22 +11,64 @@ namespace Business.Models
         public Board()
         {
             FullEmpty();
-            Player_1 = new Player(TypePiece.circle, "pepe");
-            Player_2 = new Player(TypePiece.cross, "jose");
         }
 
-        public Player Player_1 { get; set; }
-        public Player Player_2 { get; set; }
         public TypePiece stateCell { get; set; }
-        public TypePiece[] Positions { get; init; } 
+        private TypePiece[] Positions { get; init; } 
 
         public void FullEmpty()
         {
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < 9; i++)
             {
                 Positions[i] = TypePiece.empty;               
             }
         }
         
+        public void VerifiedPosition(int position) // validar la position no este avcia
+        {
+            if (position >= 0 && position <= 8)
+            {
+                throw new Exception("Posicion invalida");
+            }
+
+        }
+
+        public bool SetCellBusy(Player player, int numberCell) // 
+        {
+            if (Positions[numberCell] != TypePiece.empty)
+            {
+                return false;
+            }
+
+            Positions[numberCell] = player.TypePlayer;
+            return true;
+        }
+
+        public bool FullBoard()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (Positions[i] == TypePiece.empty)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsVertical()
+        {
+            return true;
+        }
+
+        public bool IsLHorizontal()
+        {
+            return true;
+        }
+
+        public bool IsLDiagonal()
+        {
+            return true;
+        }
     }
 }
--- TicTacToe_API/Business/Models/Game.cs
+++ TicTacToe_API/Business/Models/Game.cs
@@ -13,20 +13,55 @@ namespace Business.Models
 
         public Game()
         {
-            _boardGame = new Board();
-            _currentPlayer = _boardGame.Player_1;
+
         }
+
+
+        public Player Player_1 { get; set; }
+        public Player Player_2 { get; set; }
 
         public int Turn { get; set; } // revisar nombre de la variable
 
-        public void Move(int position, Player player)
+        public void Move(int position, Player player) // PlayTurn
         {
-            // validar si la casilla esta ocupa o no.
-            // Si esta libre guardar la posicion
-            var result = SetCellBusy(player, position);
+            try
+            {
+                _boardGame.VerifiedPosition(position);
+                
+                bool result = _boardGame.SetCellBusy(player, position);
+
+                if (result)
+                {
+                    if (!EndGane())
+                    {
+                        if (!IsThereAWinner())
+                        {
+                            _currentPlayer = GetNextPlayer();
+                        }
+                        else
+                        {
+                            // MOSTRAR MENSAJE DE QUIEN GANO
+                        }
+                    }
+                    else
+                    {
+                        // MOSTRAR EL MENSAJE DE EMPATE
+                    }
+                }
+                else
+                {
+                    // MAL MOVIMIENTO
+                    throw new Exception("Celda ocupada.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            
         }
 
-        public bool AskCellBusy()
+        public bool AskCellBusy() // programar el metodo
         {
             return true;
         }
@@ -41,70 +76,36 @@ namespace Business.Models
             return true;
         }
 
-        public bool IsVertical()
-        {
-            return true;
-        }
-
-        public bool IsLHorizontal()
-        {
-            return true;
-        }
-
-        public bool IsLDiagonal()
-        {
-            return true;
-        }
-
         public Player GetNextPlayer()
         {
-            if (_currentPlayer == _boardGame.Player_1)
+            if (_currentPlayer == Player_1)
             {
-                return _boardGame.Player_2;
+                return Player_2;
             }
             else
             {
-                return _boardGame.Player_1;
+                return Player_1;
             }
-        }
-
-        public bool SetCellBusy(Player player, int numberCell)
-        {
-            if (_boardGame.Positions[numberCell] == TypePiece.empty)
-            {
-                _boardGame.Positions[numberCell] = player.TypePlayer;
-
-                if (IsThereAWinner())
-                {
-                    // hay ganador
-                }
-                else
-                {
-                    _currentPlayer = GetNextPlayer();
-                    return true;
-                }
-            }
-            else
-            {
-                // la celda esta ocupada
-                return false;
-            }
-            return true;
         }
 
         public void StartGane()
         {
+            InitGame();
+            _currentPlayer = Player_1;
+
+            Player_1 = new Player(TypePiece.circle, "pepe");
+            Player_2 = new Player(TypePiece.cross, "jose");
 
         }
 
-        public void ResetGame()
+        public void InitGame()
         {
-            _boardGame.FullEmpty();
+            _boardGame = new Board();
         }
 
-        public void EndGane()
+        public bool EndGane()
         {
-
+            return _boardGame.FullBoard();
         }
     }
 }
